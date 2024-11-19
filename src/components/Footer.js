77596572e0ee729162e4e613c96e2c542bc9c662
yoosuhyeon <dbtngus0831@gamil.com>
2024-11-19 14:41:46 +0900

    test publish

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,6 +8,7 @@ function Footer() {
         <a
           href="https://unsplash.com/ko/%EC%82%AC%EC%A7%84/%EC%97%94%ED%84%B0%ED%85%8C%EC%9D%B8%EB%A8%BC%ED%8A%B8-%EC%84%BC%ED%84%B0-%EC%9C%84%EC%97%90-%EB%86%93%EC%9D%B8-%ED%8F%89%EB%A9%B4-tv-cPwnUGSdl-c"
           target="_blank"
+          rel="noreferrer"
         >
           배경 출처
         </a>
